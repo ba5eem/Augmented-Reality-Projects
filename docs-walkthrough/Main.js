dc3a5834211b2cb8f1580b86acdff5b61d453edd
baseem <baseem@gmail.com>
2018-12-18 16:56:55 -1000

    raycaster learning - can move object around -based off touch

--- docs-walkthrough/Main.js
+++ docs-walkthrough/Main.js
@@ -3,6 +3,7 @@ import { AR, Asset } from 'expo';
 import ExpoTHREE, { AR as ThreeAR, THREE } from 'expo-three';
 import { View as GraphicsView } from 'expo-graphics';
 import * as lib from './libs';
+import * as ARUtils from './ar-utils';
 import {AR as ArrayMethods} from './AR-Object-Methods';
 import TouchableView from './TouchableView';
 import { clear } from './ClearScreen.js';
@@ -20,20 +21,21 @@ export default class MainScreen extends React.Component {
   touch = new THREE.Vector2();
   raycaster = new THREE.Raycaster();
 
-  updateTouch = (e) => {
-    let x = e.locationX;
-    let y = e.locationY;
-    console.log(Object.keys(e))
+  updateTouch = ({ x, y }) => {
+
     this.touch.x = x / 375 * 2 - 1;
     this.touch.y = -(y / 812) * 2 + 1;
-    //this.runHitTest();
+    this.runHitTest();
   };
 
   runHitTest = () => {
     this.raycaster.setFromCamera(this.touch, this.camera);
     const intersects = this.raycaster.intersectObjects(this.planes.children);
+    console.log(this.touch);
+    this.sphere.position.set(this.touch.x, this.touch.y+2, -10);
     for (const intersect of intersects) {
       const { distance, face, faceIndex, object, point, uv } = intersect;
+      console.log(point)
       this.sphere.position.set(point.x, point.y, point.z);
       this.sphere.visible = true;
     }
@@ -53,10 +55,10 @@ export default class MainScreen extends React.Component {
     return (
       <TouchableView
         style={{ flex: 1 }}
-        onTouchesBegan={(e)=>this.updateTouch(e)}
-        onTouchesMoved={({ locationX, locationY }) =>
+        onTouchesBegan={({ locationX, locationY }) =>
           this.updateTouch({ x: locationX, y: locationY })}
-        onTouchesEnded={() => console.log('ended')}>
+        onTouchesMoved={({ locationX, locationY }) =>
+          this.updateTouch({ x: locationX, y: locationY })}>
         <GraphicsView
           style={{ flex: 1 }}
           onContextCreate={this.onContextCreate}
@@ -88,21 +90,35 @@ export default class MainScreen extends React.Component {
     this.camera = new ThreeAR.Camera(width, height, 0.01, 1000);
     console.log(width, height)
 
-    const inch = 0.0254;
-    const geometry = new THREE.BoxGeometry(2, 2, 2);
-    // Simple color material
-    const material = new THREE.MeshPhongMaterial({
-      color: 0xff00ff,
-    });
+
+    // const geometry = new THREE.BoxGeometry(2, 2, 2);
+    // // Simple color material
+    // const material = new THREE.MeshPhongMaterial({
+    //   color: 0xff00ff,
+    // });
     
-    // Combine our geometry and material
-    this.cube = new THREE.Mesh(geometry, material);
-    // Add the cube to the scene
-    this.cube.position.z = -10;
-    this.scene.add(this.cube);
-    this.cube = this.cube;
+
+    this.setupARUtils();
+    this.setupBall();
+  };
 
 
+  setupBall = () => {
+    var geometry = new THREE.SphereBufferGeometry(1, 1, 1);
+    var material = new THREE.MeshBasicMaterial({ color: 0xff00ff });
+    var sphere = new THREE.Mesh(geometry, material);
+    sphere.visible = true;
+
+    this.scene.add(sphere);
+    this.sphere = sphere;
+    this.sphere.position.z = -10;
+  };
+
+  setupARUtils = () => {
+    this.points = new ARUtils.Points();
+    this.scene.add(this.points);
+    this.planes = new ARUtils.Planes();
+    this.scene.add(this.planes);
   };
 
 
